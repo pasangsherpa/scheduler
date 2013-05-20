25b98b6f153c9f6d3b7719b5f2b4a06b2892ed7d
Mars Gokturk <mars.gokturk@gmail.com>
2013-05-20 05:01:14 -0700

    Cpu.c and cpu.h edit. Much simpler version than what I thought it would be.I will try to finish this very quickly.

--- cpu.c
+++ cpu.c
@@ -14,21 +14,62 @@
 #include "global.h"
 #endif
 
-CPUPtr CPUConstructer(int no_processes, int prod_cons_pair, int cycle) {
-	CPUPtr cpu = (CPUPtr) malloc(sizeof(CPUStr));
-	cpu->PC = 0;
-	cpu->memory = MemoryConstructor();
-	cpu->scheduler = SchedulerConstructor();
-	cpu->no_processes = no_processes;
-	cpu->process_list = (ProcessPtr*)(malloc(MAX_PROCESS *(sizeof(ProcessStr))));
+CPUPtr CPUConstructor(int no_processes, int prod_cons_pair, int cycle) {
+        CPUPtr cpu = (CPUPtr) malloc(sizeof(CPUStr));
+        cpu->PC = 0;
+        cpu->memory = MemoryConstructor();
+        cpu->scheduler = SchedulerConstructor(8);
+        cpu->no_processes = no_processes;
+        cpu->process_list = (ProcessPtr*)(malloc(MAX_PROCESS *(sizeof(ProcessStr))));
+		cpu->keyboard_queue = (Queue) CreateQueue(QUEUE_SIZE);
+        //Pointers to functions.
 
-	//Pointers to functions.
-
-	return cpu;
+        return cpu;
 }
+
+int keyboardServiceRequest(CPUPtr this, SchedulerPtr the_scheduler){
+		printf("Keyboard thread\n");
+		ProcessPtr pcb = getCurrentProcess(the_scheduler); //PCBPtr//	
+			
+		Enqueue(pcb, this-> keyboard_queue); //Enqueue the PCB to the I/O queue.
+		//Process state is set to BLOCKED.
+		//Process state is saved.
+		//PCB is removed from the READY queue.		
+		//upcall the scheduler
+		setCurrentProcess(the_scheduler);
+		printf("cpu disabled int,saved the state,enabled int, and moved the pcb to the Keyboard's blocked queue.\n");		
+		printf("CPU upcalled the Scheduler\n");
+		poll(this);	
+		//cpu resume
+		
+		return 0;
+}
+
+int poll(CPUPtr this){ //thread
+	char c = 'a';
+	keyboardServiceCompleted(this, c);
+	return NO_ERROR;
+}
+																
+int keyboardServiceCompleted(CPUPtr this, char c){
+	printf("CPU has been interrupted by the Keyboard Device.\n");
+	ProcessPtr pcb = FrontAndDequeue(this->keyboard_queue);
+	int result = addToReadyQueue(this->scheduler, pcb);
+	//if OK
+	printf("CPU disabled INT,saved the state of the current process to its PCB, enabled int, and moved the completed process to the ready queue.\n");
+	printf("Character from the buffer is %c\n",c);	
+	//cpu resume
+	return NO_ERROR;
+	}
 
 //Destructor.
 int CPUDestructor(CPUPtr this) {
-	free(this);
-	return NO_ERROR;
+        free(this);
+        return NO_ERROR;
+}
+
+int main (void){
+	CPUPtr cpu = CPUConstructor(6,6,6);	
+	keyboardServiceRequest(cpu, cpu->scheduler);
+	return 0;
 }
--- cpu.h
+++ cpu.h
@@ -20,35 +20,38 @@
 #include "scheduler.h"
 #endif
 
-typedef struct cpu {
-	int PC; //Program counter
-	MemoryPtr memory;
-	SchedulerPtr scheduler;
-	int no_processes; //Number of processes
-	ProcessPtr *process_list;
-	int max_step_Count; //Number of steps this CPU will run.
-	int INT; //Whether INT line is asserted or not (0,1)
-	int int_enabled; //Current state of the CPU, whether it is servicing an INT	
+#ifndef PROCESS_h
+#include "process.h"
+#endif
 
+typedef struct cpu {
+        int PC; //Program counter
+        MemoryPtr memory;
+        SchedulerPtr scheduler;
+        int no_processes; //Number of processes
+        ProcessPtr *process_list;
+        int max_step_Count; //Number of steps this CPU will run.
+		Queue keyboard_queue;
+        
 } CPUStr, *CPUPtr;
 
 // Constructor
 CPUPtr CPUConstructor(int no_processes, int prod_cons_pair, int cycle);
 
 // Destructor
-void CPUDestructor(CPUPtr this);
+int CPUDestructor(CPUPtr this);
 
 //METHODS
-int checkINT(CPUPtr this);
-int saveState(CPUPtr this, ProcessPtr currentProcess);
-int enableINT(CPUPtr this);
-int disableINT(CPUPtr this);
+
+int keyboardServiceRequest(CPUPtr this, SchedulerPtr the_pcb);
+int keyboardServiceCompleted(CPUPtr this, char the_keyboard_data);
+int poll(CPUPtr this); //thread	
 int timer_int_handler(CPUPtr this,SchedulerPtr scheduler);
-int io_int_handler(CPUPtr this); //task: IO device queue
-int io_req_trap_handler(CPUPtr this, SchedulerPtr scheduler);//task:I/O device queue
-int mutex_lock_trap_handler(CPUPtr this,MutexPtr mutex); 
-int mutex_unlock_trap_handler(CPUPtr this, ProcessPtr process); //assumption: we know which process
-																//is waiting on which lock+ a process can only be in one queue.
+
+
+//int mutex_lock_trap_handler(CPUPtr this,MutexPtr mutex); 
+//int mutex_unlock_trap_handler(CPUPtr this, ProcessPtr process); //assumption: we know which process
+                                                                                                                                //is waiting on which lock+ a process can only be in one queue.
 int cond_wait_trap_handler(CPUPtr this);//task:Condition pointer
 int cond_signal_trap_handler(CPUPtr this);
 
