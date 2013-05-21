09ca7a0e911c6111079bfb071e2af33492b89884
Seth Kramer <sethk2@uw.edu>
2013-05-20 23:09:50 -0700

    Added basic condition var

---
+++ conditionVar.c
@@ -0,0 +1,36 @@
+/*
+ * conditionVar.c
+ *
+ *  Created on: May 14, 2013
+ *
+ *  @author: Pasang Sherpa
+ *  @author: Seth Kramer
+ *  @author: Mars Gokturk
+ *
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include "global.h"
+ #include "queue.h"
+#include "mutex.h"
+#include "conditionVar.h"
+
+/*
+* ConditionVarPtr ConditionVarConstructor()
+* Basic Constructor for a Condition Variable object.
+*/
+ConditionVarPtr ConditionVarConstructor() {
+	ConditionVarPtr conditionVar = (ConditionVarPtr) malloc(sizeof(ConditionVar));
+	conditionVar -> mutex = (MutexPtr) MutexConstructor();
+
+	conditionVar -> wait_queue = (Queue) CreateQueue(QUEUE_SIZE);
+
+	return conditionVar;
+}
+
+void ConditionVarDestructor(ConditionVarPtr this) {
+	DisposeQueue(this -> mutex_queue);
+	MutexDestructor(this -> mutex);
+	free(this);
+}
