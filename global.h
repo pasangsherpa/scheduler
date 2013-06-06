80b5aa5b69e6904a3101e0c80c976242b6401527
Seth Kramer <sethk2@uw.edu>
2013-06-05 22:34:49 -0700

    made a key thread.... yay

--- global.h
+++ global.h
@@ -18,7 +18,7 @@
 #define ERROR				-1
 #define QUEUE_SIZE 			50
 #define MAX_PROCESS			50
-#define QUANTA	4
+#define QUANTA	8
 #define IO_QUANTA_LOWER 0.1
 #define IO_QUANTA_UPPER 0.5
 typedef enum { false, true } bool;
